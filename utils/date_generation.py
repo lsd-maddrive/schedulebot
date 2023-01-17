dc1f1b00c693ce43e10fc28e7a6e60a497f02f4d
Artyem <artyem.nagin@mail.ru>
2023-01-17 05:19:48 +0300

    [create_data] Corrected version

--- utils/date_generation.py
+++ utils/date_generation.py
@@ -2,9 +2,16 @@ from datetime import date
 
 
 def get_date_string() -> str:
-    """This function creates a date:
-    Arguments:
-        [None]
-    Returns:
-        [str] -- Returns the current date in Year-Month-Day format"""
+    """_summary_
+        This function creates a date
+
+    Parameters
+    ----------
+    [None]
+
+    Returns
+    -------
+    _type_ [str]
+        Returns the current date in Year-Month-Day format
+    """
     return date.today().strftime("%Y-%m-%d")
