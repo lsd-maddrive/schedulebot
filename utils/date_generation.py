5f2783d5cea5e36f6b3253f697a19628650f9836
Elena Serykh <elena.serykh@waveaccess.ru>
2023-01-17 09:36:28 +0300

    [RF]: minor fix in doc-string

--- utils/date_generation.py
+++ utils/date_generation.py
@@ -2,16 +2,11 @@ from datetime import date
 
 
 def get_date_string() -> str:
-    """_summary_
-        This function creates a date
-
-    Parameters
-    ----------
-    [None]
+    """This function creates a date.
 
     Returns
     -------
-    _type_ [str]
+    str
         Returns the current date in Year-Month-Day format
     """
     return date.today().strftime("%Y-%m-%d")
