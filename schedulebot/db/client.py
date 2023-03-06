35284ffa2b18ba8f9ccdbd6ed7a7f45f018c4076
Artyem <artyem.nagin@mail.ru>
2023-03-06 13:08:44 +0300

    [#37] redacted 37_DB_Client

--- schedulebot/db/client.py
+++ schedulebot/db/client.py
@@ -6,18 +6,16 @@ from dotenv import load_dotenv
 
 
 class DatabaseClient():
+    key = 'DB_DPATH'
+
     def __init__(self):
         self._logger = logging.getLogger(self.__class__.__name__)
-
         load_dotenv()
-        key = 'DB_DPATH'
-        self.DB_DPATH = os.getenv(key)
-
-        if self.DB_DPATH is None:
+        self.db_dpath = os.getenv(self.key)
+        if self.db_dpath is None:
             raise ValueError("DB_DPATH = None")
-        else:
-            os.makedirs(self.DB_DPATH, exist_ok=True)
-            PATH_TO_DB = os.path.join(self.DB_DPATH, 'data.db')
-        self.sqlite_filepath = PATH_TO_DB
+        os.makedirs(self.db_dpath, exist_ok=True)
+        self.sqlite_filepath = os.path.join(self.db_dpath, 'data.db')
         engine = sql.create_engine(f"sqlite:///{self.sqlite_filepath}")
         self._session = sql.sessionmaker(bind=engine)
+        self._logger.info(f"The engine {self.sqlite_filepath} was created successfully")
