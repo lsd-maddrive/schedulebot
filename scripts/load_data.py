c994795024d5a95b99adacc48391aacbedf7824e
Artyem <artyem.nagin@mail.ru>
2023-03-21 14:08:49 +0300

    [#38] Check db

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -2,12 +2,10 @@ import logging
 import os
 
 # import click
-import numpy as np
 import pandas as pd
 
 from schedulebot.db.client import DatabaseClient
 from schedulebot.db.models import Qualification
-from schedulebot.utils.data import parse_subject_name
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("database_loading")
@@ -21,20 +19,16 @@ def main(version: str):
     CURRENT_DPATH = os.path.dirname(os.path.abspath(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DPATH, os.pardir))
     DATA_DPATH = os.path.join(PROJECT_ROOT, "data")
-    data_version = '2023-02-26'
-    src_dpath = os.path.join(DATA_DPATH, data_version)
-    fpath = os.path.join(src_dpath, "Teachers+Lessons.csv")
+#    data_version = '2023-02-26'
+#    src_dpath = os.path.join(DATA_DPATH, data_version)
+#    fpath = os.path.join(src_dpath, "Teachers+Lessons.csv")
     if DATA_DPATH is None:
         raise ValueError("DATA_DPATH = None")
     dataf = DatabaseClient()
-    dataframe = pd.read_csv(fpath, index_col=0)
-    parsed_teachers_name = dataframe['TEACHERS'].apply(parse_subject_name).tolist()
-    parsed_teachers_name = np.array(parsed_teachers_name).reshape(-1, 2)
+#    dataframe = pd.read_csv(fpath, index_col=0)
+    dataframe = pd.DataFrame(data={'name': ["professor", "assistant"]})
 
-    dataframe['name'] = parsed_teachers_name[:, 0]
-    dataframe['qualification'] = parsed_teachers_name[:, 1]
-
-    dataf.add_df(df=dataframe['name'], table_name=Qualification.__tablename__)
+    dataf.add_df(df=dataframe, table_name=Qualification.__tablename__)
 
 
 if __name__ == "__main__":
