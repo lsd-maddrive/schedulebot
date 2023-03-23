650d78c02dd6553ead2e6ff4d7e5ade087963ab7
Artyem <artyem.nagin@mail.ru>
2023-03-23 16:03:49 +0300

    [#38] Remake

--- schedulebot/db/client.py
+++ schedulebot/db/client.py
@@ -22,9 +22,21 @@ class DatabaseClient():
         self.sqlite_filepath = os.path.join(self.db_dpath, 'data.db')
         self._engine = sql.create_engine(f"sqlite:///{self.sqlite_filepath}")
         self._session = sessionmaker(bind=self._engine)
+        metadata.create_all(self._engine)
         self._logger.info(f"The engine {self.sqlite_filepath} was created successfully")
 
     def add_df(self, df, table_name, if_exist="append"):
+        """Creates a file and adds data from df to it
+
+        Parameters
+        ----------
+        df : DataFrame
+            data
+        table_name : str
+            table title
+        if_exist : str, optional
+            if the table already exists, insert new values to the existing table,
+            by default "append"
+        """
         with self._engine.begin() as connection:
-            metadata.create_all(self._engine)
-            df.to_sql(name=table_name, con=connection, if_exists='append', index=False)
+            df.to_sql(name=table_name, con=connection, if_exists=if_exist, index=False)
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -6,35 +6,38 @@ import numpy as np
 import pandas as pd
 
 from schedulebot.db.client import DatabaseClient
-from schedulebot.db.models import Qualification
+from schedulebot.db.models import Qualification  # , Weekdays
 from schedulebot.utils.data import parse_subject_name
+
+# from schedulebot.utils.load import weekdays
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("database_loading")
+CURRENT_DPATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DPATH, os.pardir))
+DATA_DPATH = os.path.join(PROJECT_ROOT, "data")
 
 
 @click.command()
 @click.option("--version", required=True, help="The name of the data folder.")
 def main(version: str):
-    CURRENT_DPATH = os.path.dirname(os.path.abspath(__file__))
-    PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DPATH, os.pardir))
-    DATA_DPATH = os.path.join(PROJECT_ROOT, "data")
-
-    if DATA_DPATH is None:
-        raise ValueError("DATA_DPATH = None")
-
     data_version = version
     src_dpath = os.path.join(DATA_DPATH, data_version)
     fpath = os.path.join(src_dpath, "Teachers+Lessons.csv")
+    dataframe = pd.read_csv(fpath, usecols=['TEACHERS'])
 
-    dataframe = pd.read_csv(fpath, index_col=0, usecols=['TEACHERS'])
     parsed_teachers_name = dataframe['TEACHERS'].apply(parse_subject_name).tolist()
     parsed_teachers_name = np.array(parsed_teachers_name).reshape(-1, 2)
+
     dataframe['name'] = parsed_teachers_name[:, 0]
     dataframe['qualification'] = parsed_teachers_name[:, 1]
 
-    dataf = DatabaseClient()
-    dataf.add_df(df=dataframe['qualification'].unique(), table_name=Qualification.__tablename__)
+    df = pd.DataFrame(dataframe['qualification'].unique(), columns=['name'])
+    db_client = DatabaseClient()
+    db_client.add_df(df=df, table_name=Qualification.__tablename__)
+
+#    data_weekdays = DatabaseClient()
+#    data_weekdays.add_df(df=weekdays(), table_name=Weekdays.__tablename__)
 
 
 if __name__ == "__main__":
