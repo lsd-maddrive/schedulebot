511f33729b0cc2f96980e7525566a5fc2549f90f
Artyem <artyem.nagin@mail.ru>
2023-04-03 13:11:20 +0300

    [#42] 41 task corrected

--- schedulebot/utils/load.py
+++ schedulebot/utils/load.py
@@ -1,4 +1,7 @@
-def weekdays():
+from typing import List
+
+
+def weekdays() -> List[str]:
     weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     df = weekdays
     return (df)
