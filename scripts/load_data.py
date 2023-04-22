4c5d7f14e8d49e8a95b3dcb2fd2f7554e34f4b0c
Artyem <artyem.nagin@mail.ru>
2023-04-22 17:58:46 +0300

    [#44] Filling_the_table_time_interval

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -6,8 +6,9 @@ import numpy as np
 import pandas as pd
 
 from schedulebot.db.client import DatabaseClient
-from schedulebot.db.models import Qualification
+from schedulebot.db.models import Qualification, Time_interval
 from schedulebot.utils.data import parse_subject_name
+from schedulebot.utils.load import get_time_intervals
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("database_loading")
@@ -34,6 +35,8 @@ def main(version: str):
 
     db_client = DatabaseClient()
     db_client.add_df(df=df_client, table_name=Qualification.__tablename__)
+    df_time_interval = get_time_intervals()
+    db_client.add_df(df=df_time_interval, table_name=Time_interval.__tablename__)
 
 
 if __name__ == "__main__":
