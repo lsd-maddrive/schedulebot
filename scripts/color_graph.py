30f480047df15e770039609333f1b32c03948930
Elena Serykh <elena.serykh@waveaccess.ru>
2023-05-18 12:03:35 +0300

    [#100] [RF]: a little bit of refactoring

--- scripts/color_graph.py
+++ scripts/color_graph.py
@@ -1,79 +1,84 @@
+import logging
 import os
-import pickle
 import random
+from time import time
 
 import matplotlib.pyplot as plt
 import numpy
 import pandas as pd
 import seaborn as sns
-import yaml
 from deap import base, creator, tools
-from yaml.loader import SafeLoader
 
 from schedulebot.genetic import elitism, graphs
+from schedulebot.utils.fs import load_graph, read_yaml
 from schedulebot.utils.load import get_time_intervals, graph_nodes_1week, remake_str, weekdays
-
-# from scripts.debug_graphs import G
-
-# read yaml config
-CURRENT_DPATH = os.path.dirname(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.join(os.path.join(CURRENT_DPATH, 'configs'))
-fpath = os.path.join(PROJECT_ROOT, "color_graph.yaml")
-with open(fpath) as f:
-    config_constant = yaml.load(f, Loader=SafeLoader)
-    print(config_constant)
 
 CURRENT_DPATH = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DPATH, os.pardir))
 DATA_DPATH = os.path.join(PROJECT_ROOT, "data")
-src_path = os.path.join(DATA_DPATH, "graph")
-
-week1_pickle = os.path.join(src_path, "1week.pickle")
-with open(week1_pickle, 'rb') as f:
-    G = pickle.load(f)
+CONFIG_DPATH = os.path.join(PROJECT_ROOT, "configs")
 
 
-# set the random seed:
-random.seed(config_constant['RANDOM_SEED'])
-
-toolbox = base.Toolbox()
-
-# create the graph coloring problem instance to be used:
-gcp = graphs.GraphColoringProblem(G, config_constant['HARD_CONSTRAINT_PENALTY'])
-
-
-# define a single objective, maximizing fitness strategy:
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-
-# create the Individual class based on list:
-creator.create("Individual", list, fitness=creator.FitnessMin)
-
-# create an operator that randomly returns an integer in the range of participating colors:
-toolbox.register("Integers", random.randint, 0, config_constant['MAX_COLORS'] - 1)
-
-# create the individual operator to fill up an Individual instance:
-toolbox.register("individualCreator", tools.initRepeat, creator.Individual, toolbox.Integers, len(gcp))
-
-# create the population operator to generate a list of individuals:
-toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
-
-
-# fitness calculation: cost of the suggested olution
-def getCost(individual):
-    return gcp.getCost(individual),  # return a tuple
-
-
-toolbox.register("evaluate", getCost)
-
-# genetic operators:
-toolbox.register("select", tools.selTournament, tournsize=2)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutUniformInt, low=0,
-                 up=config_constant['MAX_COLORS'] - 1, indpb=1.0 / len(gcp))
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__file__)
 
 
 # Genetic Algorithm flow:
 def main():
+    # read yaml config
+    fpath = os.path.join(CONFIG_DPATH, "color_graph.yaml")
+    config_constant = read_yaml(fpath)
+    logger.info(f"Config: {config_constant}")
+
+    graph_dpath = os.path.join(DATA_DPATH, "graph")
+    week1_pickle = os.path.join(graph_dpath, "1week.pickle")
+    G = load_graph(week1_pickle)
+
+    # set the random seed:
+    random.seed(config_constant['RANDOM_SEED'])
+
+    toolbox = base.Toolbox()
+
+    # create the graph coloring problem instance to be used:
+    gcp = graphs.GraphColoringProblem(G, config_constant['HARD_CONSTRAINT_PENALTY'])
+
+    # define a single objective, maximizing fitness strategy:
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+
+    # create the Individual class based on list:
+    creator.create("Individual", list, fitness=creator.FitnessMin)
+
+    # create an operator that randomly returns an integer in the range of participating colors:
+    toolbox.register("Integers", random.randint, 0, config_constant['MAX_COLORS'] - 1)
+
+    # create the individual operator to fill up an Individual instance:
+    toolbox.register(
+        "individualCreator", tools.initRepeat, creator.Individual, toolbox.Integers, len(gcp)
+    )
+
+    # create the population operator to generate a list of individuals:
+    toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
+
+    # fitness calculation: cost of the suggested olution
+    def getCost(individual) -> tuple:
+        # NOTE: return a tuple
+        return (gcp.getCost(individual),)
+
+    toolbox.register("evaluate", getCost)
+
+    # genetic operators:
+    toolbox.register("select", tools.selTournament, tournsize=2)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register(
+        "mutate",
+        tools.mutUniformInt,
+        low=0,
+        up=config_constant['MAX_COLORS'] - 1,
+        indpb=1.0 / len(gcp)
+    )
 
     # create initial population (generation 0):
     population = toolbox.populationCreator(n=config_constant['POPULATION_SIZE'])
@@ -86,12 +91,16 @@ def main():
     # define the hall-of-fame object:
     hof = tools.HallOfFame(config_constant['HALL_OF_FAME_SIZE'])
 
+    # from time import time
+
+    start_ts = time()
     # perform the Genetic Algorithm flow with elitism:
     population, logbook = elitism.eaSimpleWithElitism(population, toolbox,
                                                       cxpb=config_constant['P_CROSSOVER'],
                                                       mutpb=config_constant['P_MUTATION'],
                                                       ngen=config_constant['MAX_GENERATIONS'],
                                                       stats=stats, halloffame=hof, verbose=True)
+    logger.info(f"Genetic algorithm is completed: {round(time() - start_ts, 3)} s")
 
     # print info for best solution found:
     best = hof.items[0]
@@ -110,12 +119,11 @@ def main():
     minFitnessValues, meanFitnessValues = logbook.select("min", "avg")
 
     weekday = weekdays()
-    time_interval = get_time_intervals()
     color_dicrionary = {}
     index = 1
     for day in weekday:
-        for time in time_interval:
-            color_dicrionary[index] = day + ' ' + time
+        for time_slot in get_time_intervals():
+            color_dicrionary[index] = day + ' ' + time_slot
             index += 1
 
     nodes = graph_nodes_1week()
